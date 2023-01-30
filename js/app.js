3678ab54a5008ae27bd7242c376683b7c67d8944
ulugbekhusainov <husainov078@gmail.com>
2023-01-30 22:05:19 +0500

    cat

---
+++ js/app.js
@@ -0,0 +1,35 @@
+const navAnim = () => {};
+// navAnim()
+const buttons = document.querySelector(".buttons");
+const headerInner = document.querySelector(".header__inner-wrapper");
+const cover = document.querySelector(".cover");
+
+// buttons.addEventListener("click", (e) => {
+//   headerInner.classList.toggle("show-nav");
+
+//   buttons.classList.toggle("toggle");
+
+//   console.log(e.target);
+//   cover.classList.toggle("show-cover");
+// });
+
+// buttons.addEventListener("click", (e) => {
+//   buttons.classList.toggle("toggle");
+//   cover.classList.toggle("show-nav");
+//   console.log(e.target);
+//   headerInner.classList.toggle("show-nav");
+// });
+
+// // buttons.children
+// console.log(buttons.children);
+// // JS;
+// const headerToggleBtn =document.querySelector('.header__toggle-btn');
+// const headerToggleBtnMenu =document.querySelector('#menu');
+// const sitenav =document.querySelector('.sitenav');
+// const overlay = document.querySelector('.overlay');
+
+// headerToggleBtn.addEventListener('click', () => {
+//   sitenav.classList.toggle('show-nav');
+//   overlay.classList.toggle('show-nav');
+//   menu.classList.toggle('bx-x');
+// })
---
+++ js/dist/app.dev.js
@@ -0,0 +1,22 @@
+// const navAnim = () => {
+//   const buttons = document.querySelector(".buttons");
+//   const headerInnerWrapper = document.querySelector(".header__inner-wrapper");
+//   const cover = document.querySelector(".cover");
+//   burger.addEventListener("click", () => {
+//     headerInnerWrapper.classList.toggle("show-nav");
+//     buttons.classList.toggle("toggle");
+//     cover.classList.toggle("show-cover");
+//   });
+// };
+// navAnim();
+// JS;
+// const headerToggleBtn =document.querySelector('.header__toggle-btn');
+// const headerToggleBtnMenu =document.querySelector('#menu');
+// const sitenav =document.querySelector('.sitenav');
+// const overlay = document.querySelector('.overlay');
+// headerToggleBtn.addEventListener('click', () => {
+//   sitenav.classList.toggle('show-nav');
+//   overlay.classList.toggle('show-nav');
+//   menu.classList.toggle('bx-x');
+// })
+"use strict";
